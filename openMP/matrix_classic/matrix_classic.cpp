6bdd2567bc5f1be1c0aca9606d20e84a09bcf930
PaveLiArcH <pakx86@gmail.com>
2013-01-03 16:07:29 +0400

    Обновлена общая часть лабораторной по OpenMP
    Signed-off-by: PaveLiArcH <pakx86@gmail.com>

---
+++ openMP/matrix_classic/matrix_classic.cpp
@@ -0,0 +1,57 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <Windows.h>
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+const int N=2048;
+const int N2=N*N;
+
+const int g_LIMIT_VALUE=100;
+
+int A[N][N], B[N][N], C[N][N];
+
+int main()
+{
+	LARGE_INTEGER _start, _end;
+	QueryPerformanceCounter(&_start);
+	
+	for(int i=0; i<N; i++)
+	{
+		for (int j=0; j<N; j++)
+		{
+			A[i][j]=rand()%g_LIMIT_VALUE;
+			B[i][j]=rand()%g_LIMIT_VALUE;
+		}
+	}
+
+	QueryPerformanceCounter(&_end);
+	auto _result=_end.QuadPart-_start.QuadPart;
+	
+	cout<<"Generation time: "<<_result<<endl;
+
+	QueryPerformanceCounter(&_start);
+
+	for(int i=0; i<N; i++)
+	{
+		for (int j=0; j<N; j++)
+		{
+			int _temp=0;
+			for (int r=0; r<N; r++)
+			{
+				_temp+=A[i][r]*B[r][j];
+			}
+			C[i][j]=_temp;
+		}
+	}
+
+	QueryPerformanceCounter(&_end);
+	_result=_end.QuadPart-_start.QuadPart;
+
+	cout<<"Multiplication time: "<<_result<<endl;
+	system("pause");
+}
