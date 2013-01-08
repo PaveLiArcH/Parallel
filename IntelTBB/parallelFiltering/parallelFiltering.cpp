14d7bcf07b03ccd76b339fc302398b6aea29231d
PaveLiArcH <pakx86@gmail.com>
2013-01-08 16:47:50 +0400

    Реализация pipeline
    Signed-off-by: PaveLiArcH <pakx86@gmail.com>

--- IntelTBB/parallelFiltering/parallelFiltering.cpp
+++ IntelTBB/parallelFiltering/parallelFiltering.cpp
@@ -17,24 +17,25 @@
 
 using namespace cv;
 
-Mat negative(Mat a_source)
+Mat* negative(Mat* a_source)
 {
-	Mat _retImage;
-	bitwise_not(a_source, _retImage);
+	Mat* _retImage=new Mat();
+	bitwise_not(*a_source, *_retImage);
 	return _retImage;
 }
 
-Mat edgeDetection(Mat a_source)
+Mat* edgeDetection(Mat* a_source)
 {
+	Mat *_retImage=new Mat();
 	Mat _gx = (Mat_<double>(3,3) << -1, 0, 1, -1, 0, 1, -1, 0, 1);
 	Mat _gy = (Mat_<double>(3,3) << -1, -1, -1, 0, 0, 0, 1, 1, 1);
-	Mat _gxI, _gyI, _retImage, _gxIF, _gyIF, _retImageF;
-	filter2D(a_source, _gxI, -1, _gx);
-	filter2D(a_source, _gyI, -1, _gy);
+	Mat _gxI, _gyI, _gxIF, _gyIF, _retImageF;
+	filter2D(*a_source, _gxI, -1, _gx);
+	filter2D(*a_source, _gyI, -1, _gy);
 	_gxI.convertTo(_gxIF, CV_64FC3);
 	_gyI.convertTo(_gyIF, CV_64FC3);
 	magnitude(_gxIF, _gyIF, _retImageF);
-	_retImageF.convertTo(_retImage, CV_8UC3);
+	_retImageF.convertTo(*_retImage, CV_8UC3);
 	return _retImage;
 }
 
@@ -43,45 +44,50 @@ class ImageInputFilter: public tbb::filter
 public:
 	ImageInputFilter( VideoCapture* a_capture ) : filter(serial_in_order), capture(a_capture), isVideo(true) {}
 	ImageInputFilter( Mat* a_image ) : filter(serial_in_order), frame(*a_image), isVideo(false) {}
-    ~ImageInputFilter();
 private:
     VideoCapture* capture;
 	Mat frame;
     bool isVideo;
     /*override*/ void* operator()(void*)
 	{
+		Mat* _retMat=new Mat();
 		if (isVideo)
 		{
-			*capture>>frame;
-			return &frame;
+			*capture>>*_retMat;
+			return _retMat;
 		} else
 		{
-			return &frame;
+			*_retMat=frame;
+			return _retMat;
 		}
 	}
 };
  
-//! Filter that changes each decimal number to its square.
+//! Filter that inverses colors of given image.
 class NegativeFilter: public tbb::filter
 {
 public:
 	NegativeFilter() : tbb::filter(parallel) {};
     /*override*/void* operator()( void* item )
 	{
-		Mat& input = *static_cast<Mat*>(item);
-		return &negative(input);
+		Mat* input = static_cast<Mat*>(item);
+		Mat* _out=negative(input);
+		delete input;
+		return _out;
 	}
 };
 
-//! Filter that changes each decimal number to its square.
+//! Filter that calls edge detection function on given image.
 class EdgeDetectionFilter: public tbb::filter
 {
 public:
 	EdgeDetectionFilter() : tbb::filter(parallel) {};
     /*override*/void* operator()( void* item )
 	{
-		Mat& input = *static_cast<Mat*>(item);
-		return &edgeDetection(input);
+		Mat* input = static_cast<Mat*>(item);
+		Mat* _out=edgeDetection(input);
+		delete input;
+		return _out;
 	}
 };
      
@@ -94,8 +100,10 @@ public:
 	ImageOutputFilter(std::string a_target) : tbb::filter(serial_in_order), target(a_target) {};
     /*override*/void* operator()( void* item )
 	{
-		Mat& input = *static_cast<Mat*>(item);
-		imshow(target, input);
+		Mat* input = static_cast<Mat*>(item);
+		imshow(target, *input);
+		delete input;
+		waitKey(1);
 		return NULL;
 	}
 };
@@ -105,28 +113,50 @@ int main( int argc, char** argv )
 	// инициализация планировщика задач TBB
 	tbb::task_scheduler_init init;
 
+	/// Create Windows
+	namedWindow("Original Image", 1);
+	namedWindow("New Image", 1);
+	//namedWindow("New Image #2", 1);
+
 	/// Read image given by user
 	//Mat image = imread( argv[1] );
 	Mat image = imread( "touhou-Touhou-Project-anime-Hakurei-Reimu.33p.jpg" );
+
+	imshow("Original Image", image);
+
 	Mat new_image, new_image2;
 
-	/// Do the operation new_image(i,j) = 255 - image(i,j)
+	ImageInputFilter _inFilter(&image);
+	NegativeFilter _negativeFilter;
+	EdgeDetectionFilter _edgeFilter;
+	ImageOutputFilter _outFilter("New Image");
+	// Create filters sequence when parallel_pipeline() is being run
+    //tbb::parallel_pipeline( 8, _inFilter & _negativeFilter & _outFilter );
+
+	// Create the pipeline
+    tbb::pipeline pipeline;
+
+    pipeline.add_filter( _inFilter );
+    pipeline.add_filter( _negativeFilter );
+	pipeline.add_filter( _edgeFilter );
+    pipeline.add_filter( _outFilter );
+
+	pipeline.run( 4 );
+
+	/*/// Do the operation new_image(i,j) = 255 - image(i,j)
 	new_image=negative(image);
 
 	/// Do the operation new_image(i,j) = alpha*image(i,j) + beta
 	new_image2=edgeDetection(image);
 
-	/// Create Windows
-	namedWindow("Original Image", 1);
-	namedWindow("New Image", 1);
-	namedWindow("New Image #2", 1);
+	
 
 	/// Show stuff
 	imshow("Original Image", image);
 	imshow("New Image", new_image);
 	imshow("New Image #2", new_image2);
 	imwrite("negative.jpg", new_image);
-	imwrite("edges.jpg", new_image2);
+	imwrite("edges.jpg", new_image2);*/
 
 	/// Wait until user press some key
 	waitKey();
